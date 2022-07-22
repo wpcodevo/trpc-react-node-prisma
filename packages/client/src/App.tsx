ef29d752b1ea97d33b8ffd0b192cd7df3f4cf428
Edem <ziddah.edem@gmail.com>
2022-07-22 20:22:40 +0000

    updated

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,13 +1,20 @@
+import './global.css';
 import { useState } from 'react';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { getFetch } from '@trpc/client';
+import { loggerLink } from '@trpc/client/links/loggerLink';
+import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
 import { trpc } from './utils/trpc';
-import './global.css';
 
 function AppContent() {
   const hello = trpc.useQuery(['hello']);
-  return <main className='p-2'>{JSON.stringify(hello.data, null, 2)}</main>;
+  return (
+    <div className='container mx-auto bg-ct-dark-200 rounded-xl shadow border p-8 m-10'>
+      <p className='text-3xl text-gray-700 font-bold mb-5'>Welcome!</p>
+      <p className='text-ct-blue-600 text-lg'>{hello.data?.message}</p>
+    </div>
+  );
 }
 
 function App() {
@@ -21,9 +28,17 @@ function App() {
         },
       })
   );
+  const url = 'http://localhost:8000/api/trpc';
+  const links = [
+    loggerLink(),
+    httpBatchLink({
+      maxBatchSize: 10,
+      url,
+    }),
+  ];
   const [trpcClient] = useState(() =>
     trpc.createClient({
-      url: 'http://localhost:8000/api/trpc',
+      links,
       fetch: async (input, init?) => {
         const fetch = getFetch();
         return fetch(input, {

6790c628e3e6dbadf366e3e9b85c2cf42cd2ffc6
Edem Ziddah <contact@codevoweb.com>
2022-10-26 11:11:00 +0000

    updated

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,18 @@
-import './global.css';
-import { useState } from 'react';
-import { QueryClientProvider, QueryClient } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { getFetch } from '@trpc/client';
-import { loggerLink } from '@trpc/client/links/loggerLink';
-import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
-import { trpc } from './utils/trpc';
+import "./global.css";
+import { useState } from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { getFetch } from "@trpc/client";
+import { loggerLink } from "@trpc/client/links/loggerLink";
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
+import { trpc } from "./utils/trpc";
 
 function AppContent() {
-  const hello = trpc.useQuery(['hello']);
+  const hello = trpc.sayHello.useQuery();
   return (
-    <div className='container mx-auto bg-ct-dark-200 rounded-xl shadow border p-8 m-10'>
-      <p className='text-3xl text-gray-700 font-bold mb-5'>Welcome!</p>
-      <p className='text-ct-blue-600 text-lg'>{hello.data?.message}</p>
+    <div className="container mx-auto bg-ct-dark-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
+      <p className="text-ct-blue-600 text-lg">{hello.data?.message}</p>
     </div>
   );
 }
@@ -28,24 +28,21 @@ function App() {
         },
       })
   );
-  const url = 'http://localhost:8000/api/trpc';
-  const links = [
-    loggerLink(),
-    httpBatchLink({
-      maxBatchSize: 10,
-      url,
-    }),
-  ];
+
   const [trpcClient] = useState(() =>
     trpc.createClient({
-      links,
-      fetch: async (input, init?) => {
-        const fetch = getFetch();
-        return fetch(input, {
-          ...init,
-          credentials: 'include',
-        });
-      },
+      links: [
+        httpBatchLink({
+          url: "http://localhost:8000/api/trpc",
+          fetch: async (input, init?) => {
+            const fetch = getFetch();
+            return fetch(input, {
+              ...init,
+              credentials: "include",
+            });
+          },
+        }),
+      ],
     })
   );
   return (
